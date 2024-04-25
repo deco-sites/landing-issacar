476d7375bcede96d93b4a0173e32055a56f4866a
paulo-vieira-electrolux <paulo.h.vieira@electrolux.com>
2024-04-25 00:02:18 -0300

    feat: testing implements new blocks

--- sections/ImageCarousel.tsx
+++ sections/ImageCarousel.tsx
@@ -1,14 +1,7 @@
-
-
-export interface Props{
-  text:string
+export interface Props {
+  text: string;
 }
 
-export default function ImageCaroussel({text="text"}:Props) {
-  return (
-  
-      <div>{text}</div>
-   
-    
-  );
+export default function ImageCaroussel({ text = "text" }: Props) {
+  return <div>{text}</div>;
 }
